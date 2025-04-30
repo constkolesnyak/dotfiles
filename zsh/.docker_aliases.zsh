3628fe368cbe60760434d7c5c4e9c7ffebfe4904
Konstantin Kolesnyak <constkolesnyak@gmail.com>
2025-04-30 11:21:55 +0600

    upd

--- zsh/.docker_aliases.zsh
+++ zsh/.docker_aliases.zsh
@@ -8,6 +8,7 @@ alias dcu='docker-compose up --remove-orphans --build --watch'
 alias dr='docker run'
 alias drd='docker run --tty --detach'
 alias dri='docker run --tty --interactive'
+alias driu='docker run --tty --interactive ubuntu bash'
 
 alias dsta='docker stop $(docker ps -q)'
 alias dl='docker container ls --all'
@@ -23,11 +24,12 @@ alias dt='docker tag'
 alias dirn='docker image rename'
 
 alias db='docker build . --platform linux/amd64 --tag'
-alias dbd='docker build . --platform linux/amd64 --no-cache --progress=plain --tag debug_img'
+alias dbp='docker build . --platform linux/amd64 --push --tag'
+alias dbd='docker build . --platform linux/amd64 --no-cache --progress=plain --tag debug'
 
 alias dei='docker exec --tty --interactive'
 
 alias dspr='docker system prune --all --volumes'
 
 # ACT
-alias act='act --container-architecture linux/amd64'
+alias act='act --container-architecture linux/amd64 --var-file .env'
--- zsh/.kubernetes_aliases.zsh
+++ zsh/.kubernetes_aliases.zsh
@@ -5,12 +5,20 @@ alias k='kubectl'
 alias ke='kubectl explain'
 alias kccc='kubectl config current-context'
 
-alias kgs='kubectl get services' # IPs
+alias kgsr='kubectl get services' # IPs
 alias kgp='kubectl get pods'
 alias kgn='kubectl get namespaces'
 alias kga='kubectl get all'
 alias kgd='kubectl get deployments'
+alias kgss='kubectl get statefulsets'
+alias kgsc='kubectl get secrets'
+alias kgscg='kubectl get secret --output yaml github-secrets | yq e ".stringData = (.data | with_entries(.value |= @base64d)) | del(.data)"'
 
 alias kaf='kubectl apply --filename'
+alias kadr='kubectl apply --dry-run=client --filename'
+alias kdf='kubectl diff --filename'
 
-alias kdd='describe deployment' # Events at the end
+alias kdd='kubectl describe deployment' # Events at the end
+
+alias krr='kubectl rollout restart'
+alias krs='kubectl rollout status'
