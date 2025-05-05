520d47661efdd0756ee7368b3192fde6bbe43536
Konstantin Kolesnyak <constkolesnyak@gmail.com>
2025-05-05 21:09:00 +0600

    add decrypt

--- zsh/.aliases.zsh
+++ zsh/.aliases.zsh
@@ -86,6 +86,7 @@ alias ring="say 'no more jobs'"
 alias fucking=sudo
 alias nv='nvim'
 alias encrypt='gpg --no-symkey-cache --symmetric --cipher-algo AES256'
+decrypt() {gpg --no-symkey-cache --output ${1%.gpg} --decrypt $1}
 mkd() {mkdir $1 && cd $1}
 
 mov_to_gif() {ffmpeg -i "$1.mov" -pix_fmt rgb8 -r 10 "$1.gif" && gifsicle -O3 "$1.gif" -o "$1.gif"}
