4269124e11f7748fe25e9bcebab9a9d7d032d5c4
Konstantin Kolesnyak <constkolesnyak@gmail.com>
2025-11-11 19:42:50 +0600

    upd

--- zsh/.clickhouse_aliases.zsh
+++ zsh/.clickhouse_aliases.zsh
@@ -1,5 +1,8 @@
-alias click='clickhouse'
+alias click='echo "CLICKHOUSE_CLI_MODE=$CLICKHOUSE_CLI_MODE\n" && clickhouse'
 alias clicki='click client --host $CLICKHOUSE_CLI_HOST --secure --password $CLICKHOUSE_CLI_PASSWORD'
 alias clickf='click client --host $CLICKHOUSE_CLI_HOST --secure --password $CLICKHOUSE_CLI_PASSWORD\
               --format PrettyCompact --queries-file'
-clickff() {clicki --queries-file $1 --format $2}
+
+clickff() {
+    clicki --queries-file $1 --format $2
+}
